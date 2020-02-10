744cbd172cd67e1974d275acb572c1c24696908c
Mihai <40312079+xtremer15@users.noreply.github.com>
2020-02-10 16:23:55 +0200

    Added new changes

--- JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
+++ JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 currentResult += 10 - 1 + 2;
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("The result is " + result);
+}
 
-const calcuationDescription = "(" + defaultResult + ")" + " + 10 - 1 + 2";
+add(2, 3);
+add(5, 3);
+
+const calcuationDescription = ` (${defaultResult} + 10  ) - 1 + 2`;
 outputResult(currentResult, calcuationDescription);
