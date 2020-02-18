baf4d89e19fc50df589e7f62b8a60a8ebb441f33
Mihai <40312079+xtremer15@users.noreply.github.com>
2020-02-18 15:11:06 +0200

    Add files via upload

--- JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
+++ JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult += 10 - 1 + 2;
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+
+//Gets the value from the input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-currentResult = add(2, 3);
+//Writes and outputs the operations that took place
+function outputLog(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); //Calls the method from vendro.js file
+}
 
+function add() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult += enterenedNr;
+  outputLog("+", initialValue, currentResult);
+}
 
-const calcuationDescription = ` (${defaultResult} + 10  ) - 1 + 2`;
-outputResult(currentResult, calcuationDescription);
+function subtract() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult -= enterenedNr;
+  outputLog("-", initialValue, currentResult);
+}
+function multiply() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult *= enterenedNr;
+  outputLog("*", initialValue, currentResult);
+}
+function divide() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult /= enterenedNr;
+  outputLog("/", initialValue, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
--- JS and CSS Study/JS/basics-01-starting-project/assets/scripts/test/test.js
+++ JS and CSS Study/JS/basics-01-starting-project/assets/scripts/test/test.js
@@ -13,38 +13,38 @@ function createList(array, subarray) {
   const ul = document.createElement("ul");
   ul.setAttribute("id", "dropdown");
   for (let i = 0; i < array.length; i++) {
-    const button = createElement('button', i);
+    const button = createElement("button", i);
     ul.appendChild(button);
     for (let j = 0; j < subarray.length; j++) {
-      const a = createSubElement('a',j);
-      ul.firstChild.appendChild(a);
+      const a = createSubElement("a", j);
+      ul.firstChild.appe  ndChild(a);
     }
   }
   elementToAppendAt.appendChild(ul);
 }
 
 function myFunc(elementToAdd) {
-    //document.querySelectorAll(".link").classList.toggle(elementToAdd);
+  //document.querySelectorAll(".link").classList.toggle(elementToAdd);
 }
 
 function hideMenu(elementToRemove) {
   //document.querySelectorAll(".link").classList.remove(elementToRemove);
 }
 
-function createSubElement(subElement,index){
-    const a = document.createElement(subElement);
-    a.setAttribute("class", "link");
-    a.appendChild(document.createTextNode(subArr[index]));
-    return a;
+function createSubElement(subElement, index) {
+  const a = document.createElement(subElement);
+  a.setAttribute("class", "link");
+  a.appendChild(document.createTextNode(subArr[index]));
+  return a;
 }
 
-function createElement(elementToCreate, index){
-    const element = document.createElement(elementToCreate);
-    element.setAttribute("class", "item " + index);
-    element.appendChild(document.createTextNode(options[index]));
-    element.setAttribute("onclick", "myFunc('show')");
-    element.setAttribute("onmouseleave", "hideMenu('show')");
-    return element;
+function createElement(elementToCreate, index) {
+  const element = document.createElement(elementToCreate);
+  element.setAttribute("class", "item " + index);
+  element.appendChild(document.createTextNode(options[index]));
+  element.setAttribute("onclick", "myFunc('show')");
+  element.setAttribute("onmouseleave", "hideMenu('show')");
+  return element;
 }
 
 createList(options, subArr);
