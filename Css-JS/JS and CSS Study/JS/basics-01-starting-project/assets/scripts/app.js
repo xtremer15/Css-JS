9f5d5048dd800ba0b96e5d0705454f64790e6a46
Mihai <40312079+xtremer15@users.noreply.github.com>
2020-02-20 16:47:56 +0200

    Add files via upload

--- Css-JS/JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
+++ Css-JS/JS and CSS Study/JS/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,53 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+//Gets the value from the input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+//Writes and outputs the operations that took place
+function outputLog(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); //Calls the method from vendro.js file
+}
 
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult += enterenedNr;
+  outputLog("+", initialValue, currentResult);
+  const logEntry = {
+    operation: "ADD",
+    previousResult: initialValue,
+    number: enterenedNr,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function subtract() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult -= enterenedNr;
+  outputLog("-", initialValue, currentResult);
+}
+function multiply() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult *= enterenedNr;
+  outputLog("*", initialValue, currentResult);
+}
+function divide() {
+  const enterenedNr = getUserNumberInput();
+  const initialValue = currentResult;
+  currentResult /= enterenedNr;
+  outputLog("/", initialValue, currentResult);
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
